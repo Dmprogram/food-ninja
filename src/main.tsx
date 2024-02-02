81610932d85eee834e85fcea1f8bb468dd3f421e
Corgi <dmprogramhh@mail.ru>
2024-02-02 13:00:09 +0300

    add-cart-to-persist-store

--- src/main.tsx
+++ src/main.tsx
@@ -2,21 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import { App } from './App'
 
 import './index.scss'
 
 import { ThemeProvider } from './contexts/ThemeContext/ThemeContext'
-import { store } from './redux/store'
+import { persister, store } from './redux/store'
+
+import { Spinner } from '@/components/Spinner/Spinner'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
+        <PersistGate loading={<Spinner />} persistor={persister}>
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,33 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+
+import { persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
+import persistStore from 'redux-persist/es/persistStore'
+import storage from 'redux-persist/lib/storage'
 
 import { cartReducer } from './features/cartSlice/cartSlice'
 import { categoriesReducer } from './features/categoriesSlice/categoriesSlice'
 
 import { productModalReducer } from '@/redux/features/productModalSlice/productModalSlice'
 
-export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-    categories: categoriesReducer,
-    productModal: productModalReducer,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+}
+const rootReducer = combineReducers({
+  cart: cartReducer,
+  categories: categoriesReducer,
+  productModal: productModalReducer,
 })
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+export const persister = persistStore(store)
